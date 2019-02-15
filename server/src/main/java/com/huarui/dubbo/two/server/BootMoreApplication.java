16a5d2b8ecec3c3a67e05158fa8e471dd2c8bc7f
lihui <lihui2473538988>
2019-02-15 21:10:53 +0800

    Initial commit

---
+++ server/src/main/java/com/huarui/dubbo/two/server/BootMoreApplication.java
@@ -0,0 +1,33 @@
+package com.huarui.dubbo.two.server;
+/**
+ * Created by Administrator on 2019/1/12.
+ */
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+//import org.springframework.boot.context.web.SpringBootServletInitializer; //1.x版本
+
+/**
+ * @Author:debug (SteadyJack)
+ * @Date: 2019/1/13 15:06
+ **/
+@SpringBootApplication
+@ImportResource(value = {"classpath:spring/spring-jdbc.xml","classpath:spring/spring-dubbo.xml"})
+@MapperScan(basePackages = "com.huarui.dubbo.two.model.mapper")
+public class BootMoreApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BootMoreApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BootMoreApplication.class, args);
+    }
+
+}
